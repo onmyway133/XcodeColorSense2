58405f5e3267df2ec5f009ef4f0526c83938f738
Khoa Pham <onmyway133@gmail.com>
2017-04-27 22:47:00 +0200

    Add pod Farge

--- XSEE/SourceEditorCommand.swift
+++ XSEE/SourceEditorCommand.swift
@@ -8,13 +8,35 @@
 
 import Foundation
 import XcodeKit
+import Farge
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
   func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
-    // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+    guard let selection = invocation.buffer.selections.firstObject as? XCSourceTextRange else {
+      completionHandler(nil)
+      return
+    }
+
+    let lineNumber = selection.start.line
+
+    guard lineNumber < invocation.buffer.lines.count,
+      let line = invocation.buffer.lines[lineNumber] as? String else {
+      completionHandler(nil)
+      return
+    }
+
+    let newLine = process(line: line)
+
+    invocation.buffer.lines.replaceObject(at: lineNumber, with: newLine)
 
     completionHandler(nil)
+  }
+
+  // MARK: - Helper
+
+  func process(line: String) -> String {
+    return line
   }
 
 }
