0aa5b587e4b1cc65f4859c6d185d1a4cf4148ca1
Khoa Pham <onmyway133@gmail.com>
2017-04-29 14:22:13 +0200

    Remove script.sh

--- Farge/Farge.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// Returns name for hex color
-///
-/// - Parameter hex: Can include #, must contain 6 digits
-/// - Returns: name
-public func name(hex: String) -> String? {
-  return Parser().parse(hex: hex)
-}
--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -o ./Farge/Farge.swift -k https://raw.githubusercontent.com/onmyway133/Farge/master/Sources/Farge.swift
-curl -o ./Farge/Converter.swift -k https://raw.githubusercontent.com/onmyway133/Farge/master/Sources/Converter.swift
-curl -o ./Farge/Parser.swift -k https://raw.githubusercontent.com/onmyway133/Farge/master/Sources/Parser.swift
-curl -o ./Farge/RGB.swift -k https://raw.githubusercontent.com/onmyway133/Farge/master/Sources/RGB.swift
-curl -o ./Farge/HSL.swift -k https://raw.githubusercontent.com/onmyway133/Farge/master/Sources/HSL.swift
-curl -o ./Farge/List.swift -k https://raw.githubusercontent.com/onmyway133/Farge/master/Sources/List.swift
